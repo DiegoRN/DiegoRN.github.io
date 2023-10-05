5c4968d7a54512b55d8966e9567bf6d9a15999d4
DiegoRN <83607922+DiegoRN@users.noreply.github.com>
2023-10-05 20:58:25 +0200

    All templates elements replaced

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
     
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
+        if ($(this).scrollTop() > 0) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
