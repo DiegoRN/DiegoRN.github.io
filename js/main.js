56e8df8b72aad5b35967d5299be988b0a18c138a
DiegoRN <83607922+DiegoRN@users.noreply.github.com>
2023-10-09 22:38:46 +0200

    LK Popup 1.0

--- js/main.js
+++ js/main.js
@@ -107,20 +107,54 @@
     
 })(jQuery);
 
+
 // Function to open the project popup
 function openProjectPopup() {
     var popup = document.getElementById("project-popup");
     popup.style.display = "block";
+    var popupBodies = document.getElementsByClassName("popup-body");
+    for (var i = 0; i < popupBodies.length; i++) {
+        popupBodies[i].scrollTop = 0;
+    }
+
+    var videoIframes = popup.querySelectorAll('iframe');
+
+    // Reload all video iframes
+    videoIframes.forEach(function (iframe) {
+        iframe.src = iframe.src;
+    });
+
+    document.body.style.overflow = "hidden"; // Prevent scrolling on the background content
+
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    overlay.addEventListener("click", function (event) {
+        console.log("Overlay clicked"); // Debugging line
+        if (event.target == overlay) {
+            closeProjectPopup();
+        }
+    });
 }
 
 // Function to close the project popup
 function closeProjectPopup() {
     var popup = document.getElementById("project-popup");
+
+    popup.querySelectorAll('iframe').forEach(v => { v.src = v.src });
     popup.style.display = "none";
+    resetSlide();
+
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+    document.body.style.overflow = "auto"; // Restore scrolling on the background content
 }
 
 // Add event listeners to your project buttons to open the popup
 document.querySelectorAll(".portfolio-item .btn").forEach(function(button) {
+    button.addEventListener("click", openProjectPopup);
+});
+
+document.querySelectorAll(".portfolio-img").forEach(function(button) {
     button.addEventListener("click", openProjectPopup);
 });
 
@@ -138,4 +172,85 @@ $(document).ready(function () {
     $('.project-popup .close-button').on('click', function () {
         $(this).closest('.project-popup').fadeOut();
     });
+});
+
+var currentSlide = 0;
+var slides = document.querySelectorAll('.carousel-slide');
+var dotsContainer = document.querySelector('.carousel-dots');
+
+// Create dots based on the number of slides
+slides.forEach(function (_, index) {
+    var dot = document.createElement('div');
+    dot.classList.add('carousel-dot');
+    if (index === currentSlide) {
+        dot.classList.add('active');
+    }
+    dotsContainer.appendChild(dot);
+});
+
+// Function to show the current slide and update dots
+function showSlide(index) {
+    if (index < 0) {
+        index = slides.length - 1;
+    } else if (index >= slides.length) {
+        index = 0;
+    }
+
+    slides.forEach(function (slide, slideIndex) {
+        if (slideIndex === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+
+    var dots = document.querySelectorAll('.carousel-dot');
+    dots.forEach(function (dot, dotIndex) {
+        if (dotIndex === index) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');
+        }
+    });
+
+    currentSlide = index;
+}
+
+// Add event listeners for navigation (e.g., next and previous buttons)
+document.getElementById("nextButton").addEventListener("click", function () {
+    showSlide(currentSlide + 1);
+});
+
+document.getElementById("prevButton").addEventListener("click", function () {
+    showSlide(currentSlide - 1);
+});
+
+// Add event listeners for dot navigation
+var dotButtons = document.querySelectorAll('.carousel-dot');
+dotButtons.forEach(function (dot, dotIndex) {
+    dot.addEventListener('click', function () {
+        showSlide(dotIndex);
+    });
+});
+
+
+function resetSlide() {
+    currentSlide = 0;
+    showSlide(0);
+}
+
+// Add event listeners for navigation (e.g., next and previous buttons)
+document.getElementById("nextButton").addEventListener("click", nextSlide);
+document.getElementById("prevButton").addEventListener("click", prevSlide);
+
+// Show the initial slide
+showSlide(currentSlide);
+
+var overlay = document.getElementById("overlay");
+
+overlay.addEventListener("click", function (event) {
+    console.log("Overlay clicked"); // Debugging line
+    if (event.target == overlay) {
+        closeProjectPopup();
+    }
 });
